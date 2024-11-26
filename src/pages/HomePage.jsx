084c35d750b785fe742af35377b878f6ce7da806
narumi katayama <narumikataiama@icloud.com>
2024-11-26 16:07:29 +0900

    ..

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,7 +24,7 @@ const HomePage = () => {
                 </div>
                 < GridFeatures/>
             </div>
-                < MissionBlock/>
+            < MissionBlock/>
         </div>
     );
 };
