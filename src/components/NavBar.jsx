aaeec34d2e0bcf58bfd0e00a2333688882413bdf
narumi katayama <narumikataiama@icloud.com>
2024-11-27 12:01:57 +0900

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faFacebookF,
@@ -8,93 +18,163 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import logo from "../assets/logo.svg";
 
+// Tema personalizado com cores para navbar
+const customTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#0c0c0e", // Cor do fundo da navbar
+        },
+        text: {
+            primary: "#bbbbbb", // Cor do texto e ícones
+        },
+    },
+});
+
+const pages = ["Home", "About Us", "Portfolio"];
+
 const Navbar = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
     return (
-        <nav
-            id="navbarExample"
-            className="navbar navbar-expand-lg fixed-top navbar-dark"
-            aria-label="Main navigation"
-        >
-            <div className="container">
-                <Link className="navbar-brand logo-image" to="/">
-                    <img src={logo} alt="U-Innova Logo"/>
-                </Link>
+        <ThemeProvider theme={customTheme}>
+            <AppBar position="fixed">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        {/* Logo */}
+                        <Box
+                            component="a"
+                            href="/"
+                            sx={{
+                                display: { xs: "none", md: "flex" },
+                                mr: 1,
+                                textDecoration: "none",
+                            }}
+                        >
+                            <img src={logo} alt="U-Innova Logo" style={{ height: 40 }} />
+                        </Box>
 
-                <button
-                    className="navbar-toggler p-0 border-0"
-                    type="button"
-                    id="navbarSideCollapse"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                mr: 2,
+                                display: { xs: "flex", md: "none" },
+                                flexGrow: 1,
+                                color: "inherit", // Herda a cor do texto definida no tema
+                                textDecoration: "none",
+                            }}
+                        >
+                            <img src={logo} alt="U-Innova Logo" style={{ height: 30 }} />
+                        </Typography>
 
-                <div
-                    className="navbar-collapse offcanvas-collapse"
-                    id="navbarsExampleDefault"
-                >
-                    <ul className="navbar-nav ms-auto navbar-nav-scroll">
-                        <li className="nav-item">
-                            <a
-                                className="nav-link"
-                                href="#"
-                                onClick={() => {
-                                    const target = document.getElementById('home-top');
-                                    target.scrollIntoView({behavior: 'smooth'});
-                                }}
+                        {/* Menu Mobile */}
+                        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="open menu"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                sx={{ color: "text.primary" }}
                             >
-                                Home
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link"
-                                href="#"
-                                onClick={() => {
-                                    const target = document.getElementById('about');
-                                    target.scrollIntoView({behavior: 'smooth'});
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: "bottom",
+                                    horizontal: "left",
                                 }}
-                            >
-                                About Us
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link"
-                                href="#"
-                                onClick={() => {
-                                    const target = document.getElementById('portfolio');
-                                    target.scrollIntoView({behavior: 'smooth'});
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: "top",
+                                    horizontal: "left",
                                 }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{ display: { xs: "block", md: "none" } }}
                             >
-                                Portfolio
-                            </a>
-                        </li>
-                    </ul>
+                                {/* Links */}
+                                {pages.map((page) => (
+                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                        <Typography
+                                            textAlign="center"
+                                            sx={{ color: "text.primary" }}
+                                        >
+                                            {page}
+                                        </Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
 
-                    <span className="nav-item social-icons">
-            <span className="fa-stack">
-              <a target="_blank" href="https://www.facebook.com/uinnova.studio">
-                <FontAwesomeIcon icon={faFacebookF} className="fa-stack-1x"/>
-              </a>
-            </span>
-            <span className="fa-stack">
-              <a
-                  target="_blank"
-                  href="https://www.instagram.com/uinnova_studio"
-              >
-                <FontAwesomeIcon icon={faInstagram} className="fa-stack-1x"/>
-              </a>
-            </span>
-            <span className="fa-stack">
-              <a target="_blank" href="https://wa.me/+818066557091">
-                <FontAwesomeIcon icon={faWhatsapp} className="fa-stack-1x"/>
-              </a>
-            </span>
-          </span>
-                </div>
-            </div>
-        </nav>
+                        {/* Menu Desktop + Ícones de Redes Sociais */}
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: "flex",
+                                justifyContent: "flex-end",
+                                alignItems: "center",
+                            }}
+                        >
+                            {/* Links */}
+                            <Box sx={{ display: { xs: "none", md: "flex" }, mr: 2 }}>
+                                {pages.map((page) => (
+                                    <Button
+                                        key={page}
+                                        href={`#${page.toLowerCase().replace(/\s+/g, '-')}`}
+                                        onClick={handleCloseNavMenu}
+                                        sx={{
+                                            my: 2,
+                                            color: "text.primary",
+                                            display: "block",
+                                        }}
+                                    >
+                                        {page}
+                                    </Button>
+                                ))}
+                            </Box>
+
+                            {/* Ícones de Redes Sociais */}
+                            <Box sx={{ display: "flex", alignItems: "center" }}>
+                                <IconButton
+                                    href="https://www.facebook.com/uinnova.studio"
+                                    target="_blank"
+                                    sx={{ color: "text.primary" }}
+                                >
+                                    <FontAwesomeIcon icon={faFacebookF} />
+                                </IconButton>
+                                <IconButton
+                                    href="https://www.instagram.com/uinnova_studio"
+                                    target="_blank"
+                                    sx={{ color: "text.primary" }}
+                                >
+                                    <FontAwesomeIcon icon={faInstagram} />
+                                </IconButton>
+                                <IconButton
+                                    href="https://wa.me/+818066557091"
+                                    target="_blank"
+                                    sx={{ color: "text.primary" }}
+                                >
+                                    <FontAwesomeIcon icon={faWhatsapp} />
+                                </IconButton>
+                            </Box>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </ThemeProvider>
     );
 };
 
