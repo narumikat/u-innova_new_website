dcececb2bea83cf9934c5a1a2c00ca8e33e5a9e2
narumi katayama <narumikataiama@icloud.com>
2024-11-29 17:15:25 +0900

    ..

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -1,5 +1,5 @@
-import CassianoImage from '../assets/cassiano.jpg'
-import NarumiImage from '../assets/narumi.jpg'
+import CassianoImage from '../assets/cassiano.jpg';
+import NarumiImage from '../assets/narumi.jpg';
 
 const teamMembers = [
     {
@@ -8,7 +8,7 @@ const teamMembers = [
         description:
             'Backend Software Engineer specializing in scalable management solutions for the real estate sector in Tokyo, with expertise in API development, database design, and cloud technologies.',
         alt: 'Cassiano Yasumitsu',
-        link: 'about.html',
+        link: '/about',
     },
     {
         name: 'Narumi Katayama',
@@ -16,34 +16,42 @@ const teamMembers = [
         description:
             'Full Stack Developer specializing in backend systems with AI, and experienced in web design, digital marketing, and branding. Also an influencer with thousands of followers.',
         alt: 'Narumi Katayama',
-        link: 'about.html',
+        link: '/about',
     },
 ];
 
 const AboutCards = () => {
     return (
         <section className="card-container">
+            <div className="d-flex justify-content-center align-items-center" id="about">
+                <h2 className="gradient-font2 mt-5">Who We Are</h2>
+            </div>
+            <div className="d-flex card-box">
+                {teamMembers.map((member, index) => (
+                    <a
+                        key={index}
+                        href={member.link}
+                        className="card-about"
+                        style={{
+                            textDecoration: 'none',
+                            color: 'inherit'
+                        }} // Remove o sublinhado e mantém a cor original
+                    >
+                        <div className="card-about-body">
+                            <img src={member.imgSrc} alt={member.alt}/>
+                            <div className="card-content">
+                                <h2>{member.name}</h2>
+                                <p>{member.description}
+                                    <hr/>
 
-                <div className="d-flex justify-content-center align-items-center" id="about">
-                    <h2 className="gradient-font2 mt-5">Who We Are</h2>
-                </div>
-                <div className="d-flex card-box">
-                    {teamMembers.map((member, index) => (
-                        <div key={index} className="card-about">
-                            <div className="card-about-body">
-                                <img src={member.imgSrc} alt={member.alt}/>
-                                <div className="card-content">
-                                    <h2>{member.name}</h2>
-                                    <p>{member.description}</p>
-                                    <a href='/about' className="button">
-                                        About Us
-                                    </a>
-                                </div>
+                                    Read More
+                                </p>
+
                             </div>
                         </div>
-                    ))}
-                </div>
-
+                    </a>
+                ))}
+            </div>
         </section>
     );
 };
