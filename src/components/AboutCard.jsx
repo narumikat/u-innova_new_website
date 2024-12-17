1ba0ee3fe5b5f10fda6f0f201c450f2005be3c42
narumi katayama <narumikataiama@icloud.com>
2024-12-17 11:11:02 +0900

    team members

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -1,23 +1,47 @@
 import CassianoImage from '../assets/cassiano.jpg';
 import NarumiImage from '../assets/narumi.jpg';
+import CintiaImage from '../assets/cintia2.jpeg';
+import MarceloImage from '../assets/marcelo2.png';
+import RodrigoImage from '../assets/rodrigo2.png';
 
 const teamMembers = [
-    {
-        name: 'Cassiano Yasumitsu',
-        imgSrc: CassianoImage,
-        description:
-            'Backend Software Engineer specializing in scalable management solutions for the real estate sector in Tokyo, with expertise in API development, database design, and cloud technologies.',
-        alt: 'Cassiano Yasumitsu',
-        link: '/about',
-    },
     {
         name: 'Narumi Katayama',
         imgSrc: NarumiImage,
         description:
-            'Full Stack Developer specializing in backend systems with AI, and experienced in web design, digital marketing, and branding. Also an influencer with thousands of followers.',
+            'CEO | Founder',
         alt: 'Narumi Katayama',
         link: '/about',
     },
+    {
+        name: 'Cassiano Yasumitsu',
+        imgSrc: CassianoImage,
+        description:
+            'CTO | Co-founder',
+        alt: 'Cassiano Yasumitsu',
+        link: '/about',
+    },
+    {
+        name: 'Rodrigo Toyoda',
+        imgSrc: RodrigoImage,
+        description: 'Lead Front-End Developer',
+        alt: 'Rodrigo Toyoda',
+        link: '/about',
+    },
+    {
+        name: 'Marcelo Hirata',
+        imgSrc: MarceloImage,
+        description: 'Data Analyst & Technical Translator',
+        alt: 'Marcelo Hirata',
+        link: '/about',
+    },
+    {
+        name: 'Midori Tamashiro',
+        imgSrc: CintiaImage,
+        description: 'International Communication Specialist',
+        alt: 'Cintia Tamashiro',
+        link: '/about',
+    }
 ];
 
 const AboutCards = () => {
@@ -26,30 +50,48 @@ const AboutCards = () => {
             <div className="d-flex justify-content-center align-items-center" id="about">
                 <h2 className="gradient-font2 mt-5">Who We Are</h2>
             </div>
-            <div className="d-flex card-box">
-                {teamMembers.map((member, index) => (
-                    <a
-                        key={index}
-                        href={member.link}
-                        className="card-about"
-                        style={{
-                            textDecoration: 'none',
-                            color: 'inherit'
-                        }} // Remove o sublinhado e mantém a cor original
-                    >
-                        <div className="card-about-body">
-                            <img src={member.imgSrc} alt={member.alt}/>
-                            <div className="card-content">
-                                <h2>{member.name}</h2>
-                                <p>{member.description}
-                                    <hr/>
-
-                                    Read More
-                                </p>
-
+            <div className="row justify-content-center card-box">
+                {teamMembers.slice(0, 2).map((member, index) => (
+                    <div key={index} className="col-12 col-md-2 d-flex justify-content-center">
+                        <a
+                            href={member.link}
+                            className="card-about"
+                            style={{
+                                textDecoration: 'none',
+                                color: 'inherit'
+                            }}
+                        >
+                            <div className="card-about-body">
+                                <img src={member.imgSrc} alt={member.alt}/>
+                                <div className="card-content">
+                                    <h2>{member.name}</h2>
+                                    <p>{member.description}</p>
+                                </div>
                             </div>
-                        </div>
-                    </a>
+                        </a>
+                    </div>
+                ))}
+            </div>
+            <div className="row justify-content-center mt-4 card-box">
+                {teamMembers.slice(2).map((member, index) => (
+                    <div key={index} className="col-12 col-md-3 d-flex justify-content-center">
+                        <a
+                            href={member.link}
+                            className="card-about"
+                            style={{
+                                textDecoration: 'none',
+                                color: 'inherit'
+                            }}
+                        >
+                            <div className="card-about-body">
+                                <img src={member.imgSrc} alt={member.alt}/>
+                                <div className="card-content">
+                                    <h2>{member.name}</h2>
+                                    <p>{member.description}</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
                 ))}
             </div>
         </section>
